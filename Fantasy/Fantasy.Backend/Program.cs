064b81dd2caec30e7b19f174659f918110622da8
FERNANDO FRANCO MARTINEZ <ferfranco.uy@hotmail.com>
2025-03-20 22:07:20 -0400

    Add countries API controller

--- Fantasy/Fantasy.Backend/Program.cs
+++ Fantasy/Fantasy.Backend/Program.cs
@@ -1,11 +1,20 @@
 using Fantasy.Backend.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "3.0.0",
+        Title = "Fantasy API",
+        Description = "An API for managing fantasy entities"
+    });
+});
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
@@ -13,7 +22,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fantasy API v1");
+    });
 }
 
 app.UseHttpsRedirection();
